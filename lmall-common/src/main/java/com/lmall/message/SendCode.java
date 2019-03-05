d595f7d257724f8277631c39297852a1ba6557cb
hippiie223 <13260669530@163.com>
2019-03-05 16:36:43 +0800

    common

---
+++ lmall-common/src/main/java/com/lmall/message/SendCode.java
@@ -0,0 +1,79 @@
+package com.lmall.message;
+
+import com.lmall.util.CheckSumBuilder;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by reckywangbowen_i on 2019/03/04
+ */
+public class SendCode {
+    //发送验证码的请求路径URL
+    private static final String
+            SERVER_URL="https://api.netease.im/sms/sendcode.action";
+    //网易云信分配的账号，请替换你在管理后台应用下申请的Appkey
+    private static final String
+            APP_KEY = "b30adcf093a609656cb491ca0f3e8031";
+    //网易云信分配的密钥，请替换你在管理后台应用下申请的appSecret
+    private static final String APP_SECRET = "020a1fb7e063";
+    //随机数
+    private static final String NONCE = "123456";
+    //短信模板ID
+    private static final String TEMPLATEID = "9734517";
+    //手机号
+    private static final String MOBILE="13260669530";
+    //验证码长度，范围4～10，默认为4
+    private static final String CODELEN="6";
+
+    public static String send(String phone) throws Exception {
+
+        DefaultHttpClient httpClient = new DefaultHttpClient();
+        HttpPost httpPost = new HttpPost(SERVER_URL);
+        String curTime = String.valueOf((new Date()).getTime() / 1000L);
+        /*
+         * 参考计算CheckSum的java代码，在上述文档的参数列表中，有CheckSum的计算文档示例
+         */
+        String checkSum = CheckSumBuilder.getCheckSum(APP_SECRET, NONCE, curTime);
+
+        // 设置请求的header
+        httpPost.addHeader("AppKey", APP_KEY);
+        httpPost.addHeader("Nonce", NONCE);
+        httpPost.addHeader("CurTime", curTime);
+        httpPost.addHeader("CheckSum", checkSum);
+        httpPost.addHeader("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        // 设置请求的的参数，requestBody参数
+        List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+        /*
+         * 1.如果是模板短信，请注意参数mobile是有s的，详细参数配置请参考“发送模板短信文档”
+         * 2.参数格式是jsonArray的格式，例如 "['13888888888','13666666666']"
+         * 3.params是根据你模板里面有几个参数，那里面的参数也是jsonArray格式
+         */
+        nvps.add(new BasicNameValuePair("templateid", TEMPLATEID));
+        nvps.add(new BasicNameValuePair("mobile", phone));
+        nvps.add(new BasicNameValuePair("codeLen", CODELEN));
+
+        httpPost.setEntity(new UrlEncodedFormEntity(nvps, "utf-8"));
+
+        // 执行请求
+        HttpResponse response = httpClient.execute(httpPost);
+        /*
+         * 1.打印执行结果，打印结果一般会200、315、403、404、413、414、500
+         * 2.具体的code有问题的可以参考官网的Code状态表
+         */
+        return EntityUtils.toString(response.getEntity(), "utf-8");
+
+    }
+
+
+
+}
--- lmall-common/src/main/java/com/lmall/respbody/RootRespBody.java
+++ lmall-common/src/main/java/com/lmall/respbody/RootRespBody.java
@@ -72,8 +72,29 @@ public class RootRespBody<T> {
     public enum Status {
         //状态码
         SUCCESS(200),
+        DUPLICATE_RECORD_ERROR(210),
+        RELEVANT_DATA_INEXISTENCE_ERROR(220),
+        HIVE_METASTORE_ERROR(230),
+        PERMISSION_DENIED_ERROR(240),
+        ILLEGAL_OPERATION_ERROR(250),
+        INSERT_RECORD_ERROR(260),
+        FORM_INVALID_ERROR(270),
+        INVALID_PARAM(280), // 无效参数
+        UNSUPPORTED_OPERATION(290),
+        PAGE_QUERY_PARAM_ERROR(301), //分页查询请求的参数错误
+        PROCESS_TASK_NOT_FOUND(401),
+        PROCESS_TASK_FORM_FORMAT_ERROR(402),
+        PROCESS_TASK_FORM_INTEGRITY_ERROR(403),
+        PROCESS_DELEGATE_ERROR(405),
+        REQUEST_PARAMETER_ERROR(406), //请求参数错误
+        APPLICANT_NOT_FOUND(409),    //申请人未找到
+        SYSTEM_API_ERROR(411),
+        PERMISSION_DENIED(428), //没有权限
+        DATA_ERROR(432), // 数据格式正确但语义错误
         INTERNAL_SERVER_ERROR(500),
-        BAD_REQUEST(400);
+        PROCESS_FORM_PARAM_ERROR(415), //流程表单参数错误
+        BAD_REQUEST(400),
+        COOKIE_NOT_SET(201);
         private int value;
 
         Status(int value) {
