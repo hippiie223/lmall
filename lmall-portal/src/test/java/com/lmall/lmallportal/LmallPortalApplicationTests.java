47bd35b981ac1a3229a7e2645a350f326f2d806f
hippiie223 <13260669530@163.com>
2019-03-11 21:06:13 +0800

    test

--- lmall-portal/src/test/java/com/lmall/lmallportal/LmallPortalApplicationTests.java
+++ lmall-portal/src/test/java/com/lmall/lmallportal/LmallPortalApplicationTests.java
@@ -1,17 +1,15 @@
 package com.lmall.lmallportal;
 
-import com.lmall.service.TestMessageSend;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LmallPortalApplicationTests {
-    @Autowired
-    private TestMessageSend messageSend;
+
+
 
     @Test
     public void contextLoads() {
--- lmall-user/src/test/java/com/lmall/LmallUserApplicationTests.java
+++ lmall-user/src/test/java/com/lmall/LmallUserApplicationTests.java
@@ -1,16 +1,23 @@
 package com.lmall;
 
+import com.lmall.domain.example.UsersExample;
+import com.lmall.mapper.UsersMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LmallUserApplicationTests {
-
+    @Autowired
+    private UsersMapper usersMapper;
     @Test
     public void contextLoads() {
+        UsersExample example = new UsersExample();
+        example.createCriteria().andPhoneEqualTo("13260669530");
+        System.out.println(usersMapper.selectByExample(example));
     }
 
 }
