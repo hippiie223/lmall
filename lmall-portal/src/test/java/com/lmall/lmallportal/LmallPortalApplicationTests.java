1d65cef70536c507c0950d53fd822fc097d20381
hippiie223 <13260669530@163.com>
2019-03-17 13:48:34 +0800

    exec注释

--- lmall-portal/src/test/java/com/lmall/lmallportal/LmallPortalApplicationTests.java
+++ lmall-portal/src/test/java/com/lmall/lmallportal/LmallPortalApplicationTests.java
@@ -8,9 +8,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LmallPortalApplicationTests {
-
-
-
     @Test
     public void contextLoads() {
 
